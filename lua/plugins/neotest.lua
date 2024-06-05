545f766e30a1ecc121d3d3bdf480d3af3fadaccd
Kevin C. <kevin.ceresa@xfarm.ag>
2024-06-05 09:23:37 +0200

    Add keys for neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,5 +1,21 @@
 return {
   "nvim-neotest/neotest",
+  keys = {
+    {
+      "<leader>dtd",
+      function()
+        require("neotest").run.run({ strategy = "dap" })
+      end,
+      desc = "Run test with DAP",
+    },
+    {
+      "<leader>dtt",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run nearest test",
+    },
+  },
   opts = function(_, opts)
     opts.adapters = opts.adapters or {}
     vim.list_extend(opts.adapters, {
